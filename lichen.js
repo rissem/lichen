613c2528f8b88b3e9747c20533d3f16981d8e0bd
rissem <rissem@gmail.com>
2014-09-09 14:27:48 -0700

    seed the database with null cells

--- lichen.js
+++ lichen.js
@@ -5,5 +5,15 @@ Moves = new Mongo.Collection("moves");
 Cells = new Mongo.Collection("cells");
 
 if (Meteor.isServer && Cells.find().count() == 0){
-  
+  console.log("No cells exist, creating cells");
+  for (var i = -7; i < 7; i++){
+    for (var j = -7; j < 7 ; j++){
+      Cells.insert({
+        _id: i + "," + j,
+        type: "null"
+      });
+    }
+  }
 }
+
+
