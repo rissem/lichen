a6dcb1994fbe50816e2a0762e14defe811ed084b
rissem <rissem@gmail.com>
2014-09-09 14:12:34 -0700

    towards a real project

--- lichen.js
+++ lichen.js
@@ -1,23 +1,9 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Games = new Mongo.Collection("games");
+Pieces = new Mongo.Collection("pieces");
+Players = new Mongo.Collection("players");
+Moves = new Mongo.Collection("moves");
+Cells = new Mongo.Collection("cells");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+if (Meteor.isServer && Cells.find().count() == 0){
+  
 }
